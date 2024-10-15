05df16cf021171909a48755931ed30a262ecc329
Nero N <neronn14@gmail.com>
2024-10-15 10:31:10 +0400

    new commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,22 @@
 // import React from 'react'
 
-const Product = () => {
+const Product = ({product}) => {
+  const {id,image,category,title,description,price,rating} = product;
   return (
-    <div>Product</div>
+    <div className="bg-white ring-1 ring-slate-900/5 rounded-xl group">
+        <div className="bg-white flexCenter m-1.5 py-10 rounded-xl ring-1 ring-slate-200/20 shadow-sm relative">
+            <img src={image} alt="productImg" height={122} width={122} className="object-contain aspect-square"/>
+        </div>
+        <div>
+          <h5>{category}</h5>
+          <h4>{title}</h4>
+          <p>{description}</p>
+          <div>
+            <h6>${price}</h6>
+            <p>sales ({rating.count})</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,19 +1,29 @@
-// import React from 'react'
-
 import { useContext } from "react"
 import { ProductContext } from "../context/ProductContext"
+import Product from "./Product";
 
 const Shop = () => {
   const {products} = useContext(ProductContext)
-  console.log(products)
+  // console.log(products)
+  console.log("Products:", products); 
+
+  // get only men's and women's clothing category
+  const filterProducts = products.filter((item)=>{
+    return(
+      item.category === "men's clothing" || item.category === "women's clothing"
+    )
+  })
+  console.log("Category:",filterProducts)
   return (
     <section id="shop" className="madd-padd-container py-20 bg-[#f8f7f4]">
         {/* title */}
         <h3 className="h3">Our Products</h3>
         <hr className="h-[2px] md:w-1/2 max-w-96 bg-gradient-to-l from-transparent via-black to-black mb-24 border-none rounded"/>
         {/* shop container */}
-        <div>
-            shop items
+        <div className="grid grid-cols-1 xs:grid-col-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+            {filterProducts.map((product)=>(
+              <Product product={product} key={product.id}/>
+            ))}
         </div>
     </section>
   )
