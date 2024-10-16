16f5608d8cfc6322a7f2e699be5c8f37af4d06d1
AlisahibRasulov <alisahibrasulov@gmail.com>
2024-10-16 14:07:51 +0400

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.100.15', // Geliştirme sunucusunun dinleyeceği IP adresi
+    port: 5173,             // (Opsiyonel) Kullanmak istediğiniz port
+    strictPort: true,       // (Opsiyonel) Port zaten kullanılıyorsa hata ver
+    https: false,           // (Opsiyonel) HTTPS kullanmak isterseniz true yapın
+    // Diğer server ayarları...
+  }
 })
