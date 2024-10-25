3bee4193e8f28467bbf0259f07593c6759cbe2c5
Nero N <neronn14@gmail.com>
2024-10-25 10:02:58 +0400

    new commit

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,9 +4,12 @@ import { createContext, useState } from "react"
 export const CartContext = createContext()
 
 const CartProvider = ({children}) => {
+  
 const [cart, setCart] = useState([])
 const addToCart = (product,id)=>{
-  const newItem = {...product, quantity:1}
+  console.log(`product ${id} ${product.title} added to cart`)
+  const newItem = {...product, quantity:1};
+  // console.log("newitem",newItem)
   const cartItem = cart.find((item)=>{
     return item.id === id;
   })
@@ -14,19 +17,20 @@ const addToCart = (product,id)=>{
     const newCart = [...cart].map((item)=>{
       if(item.id === id){
         return {...item, quantity:cartItem.quantity + 1}
-
+        
       }else{
         return item
       }
     })
+    // console.log("cartitem",cartItem)
     setCart(newCart)
+    console.log("newCart",newCart)
   }else{
     setCart([...cart, newItem])
+    console.log("Cart",cart,"newitem",newItem)
+    // console.log("Cart",cart)
   }
-  console.log(newItem)
-  console.log(`product ${id} ${product.title} added to cart`)
 }
-console.log(cart)
 
   return (
     <CartContext.Provider value={{addToCart}}>
